8c05c21cd90968be0e83db36a0dbc549865a7615
Azael <azaelsanchez8@gmail.com>
2023-08-21 16:20:28 -0600

    End of Section 4

--- API/Data/Migrations/20230815211212_UserPasswordAdded.Designer.cs
+++ API/Data/Migrations/20230815211212_UserPasswordAdded.Designer.cs
@@ -2,6 +2,7 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230815211212_UserPasswordAdded")]
+    partial class UserPasswordAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
@@ -22,10 +25,10 @@ namespace API.Data.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("INTEGER");
 
-                    b.Property<byte[]>("PasswordHash")
+                    b.Property<byte[]>("PaswordHash")
                         .HasColumnType("BLOB");
 
-                    b.Property<byte[]>("PasswordSalt")
+                    b.Property<byte[]>("PaswordSalt")
                         .HasColumnType("BLOB");
 
                     b.Property<string>("UserName")
@@ -33,7 +36,7 @@ namespace API.Data.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Users", (string)null);
+                    b.ToTable("Users");
                 });
 #pragma warning restore 612, 618
         }
