a399c2a54341466a6513e51fee4ca6b58080aa40
azaelDeveloper <azaelsanchez8@gmail.com>
2024-02-06 13:03:15 -0600

    Alcanzar la sesión

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.IdentityModel.Tokens;
 using API.Entities;
 using API.Interfaces;
 using System.Text;
 using System.Security.Claims;
 using System.IdentityModel.Tokens.Jwt;
-using System.Diagnostics;
 
 namespace API.Services
 {
