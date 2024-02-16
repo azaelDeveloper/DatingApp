926b010606ea1a4cc80cc938e6fc20a62646d353
azaelDeveloper <azaelsanchez8@gmail.com>
2024-02-16 10:56:13 -0600

    End of Section 5

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
+import { JsonPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -24,7 +25,18 @@ export class AccountService {
           this.currentUserSource.next(user);
         }
       })
-    );
+    )
+  }
+
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
   }
 
   setCurrentUser(user: User){
